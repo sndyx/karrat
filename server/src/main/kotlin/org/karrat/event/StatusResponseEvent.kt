bfa6ff5c926bdca49ee8fb8b3edde38eb1ab71b7
Sandy <70289658+sndyx@users.noreply.github.com>
2021-09-24 09:19:04 -0400

    Update StatusResponseEvent.kt

--- server/src/main/kotlin/org/karrat/event/StatusResponseEvent.kt
+++ server/src/main/kotlin/org/karrat/event/StatusResponseEvent.kt
@@ -1,1 +1,18 @@
-// TODO
+/*
+ * Copyright © Karrat - 2021.
+ */
+
+package org.karrat.event
+
+import org.karrat.network.Session
+import org.karrat.packet.serverbound.ServerboundPacket
+
+class StatusResponseEvent(
+    val session: Session
+    var response: StatusResponse
+) : CancellableEvent() {
+    
+    override fun toString(): String =
+        "StatusResponseEvent(session=$session, response=$response)"
+    
+}
