85119645550bfcd651ba58dbad78fb023b882f6d
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2021-10-13 14:44:28 -0700

    Potential fix

--- server/src/main/kotlin/org/karrat/packet/status/StatusResponsePacket.kt
+++ server/src/main/kotlin/org/karrat/packet/status/StatusResponsePacket.kt
@@ -7,11 +7,15 @@ package org.karrat.packet.status
 import org.karrat.packet.ClientboundPacket
 import org.karrat.struct.DynamicByteBuffer
 import org.karrat.struct.writeString
+import org.karrat.struct.writeVarInt
 
 public class StatusResponsePacket(private val response: String) : ClientboundPacket {
     
     override val id: Int = 0x00
     
-    override fun write(data: DynamicByteBuffer): Unit = data.writeString(response)
+    override fun write(data: DynamicByteBuffer) {
+        data.writeVarInt(response.length)
+        data.writeString(response)
+    }
     
 }
