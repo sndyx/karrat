14ec9d1103d5a64d3cf8a5243b583029cf5f8413
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2022-01-15 21:27:22 -0700

    chat component more like component chat

--- server/src/main/kotlin/org/karrat/play/ChatComponent.kt
+++ server/src/main/kotlin/org/karrat/play/ChatComponent.kt
@@ -4,80 +4,163 @@
 
 package org.karrat.play
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import org.karrat.entity.Entity
 
+//TODO custom optimized serialization
 @Serializable
 public sealed class ChatComponentBase {
-    public val extra: List<ChatComponent> = arrayListOf()
+    public val style: Style
+    protected val clickEvent: ClickEvent
+    protected val hoverEvent: HoverEvent
+
+    protected val attributes: TextAttributes = TextAttributes()
+
+    protected constructor(
+        style: Style,
+        clickEvent: ClickEvent,
+        hoverEvent: HoverEvent,
+    ) {
+        this.style = style
+        this.clickEvent = clickEvent
+        this.hoverEvent = hoverEvent
+    }
 }
 
+@Serializable
+public class MainChatComponent : ChatComponentBase {
+    public val extra: List<ChatComponentBase> = mutableListOf()
+
+    public constructor(style: Style,
+                       clickEvent: ClickEvent,
+                       hoverEvent: HoverEvent) : super(style, clickEvent, hoverEvent)
+}
 
 @Serializable
-public class ChatComponent : ChatComponentBase {
-    public lateinit var text: String
+public class TextComponent : ChatComponentBase {
+    public var text: String
 
-    /*private val bold: Boolean
-    private val italic: Boolean
-    private val underlined: Boolean
-    private val strikethrough: Boolean
-    private val obfuscated: Boolean
-    private val color: String
-    private val insertion: String
-    private val clickEvent: ClickEvent
-    private val hoverEvent: HoverEvent
-    */
-
-    public constructor(text: String) {
+    public constructor(text: String,
+                    style: Style,
+                    clickEvent: ClickEvent,
+                    hoverEvent: HoverEvent) : super(style, clickEvent, hoverEvent) {
         this.text = text
     }
+}
 
-    public constructor(builder: Builder.() -> Unit)
+@Serializable
+public class Style(public val bold: Boolean,
+                   public val italic: Boolean,
+                   public val underlined: Boolean,
+                   public val strikethrough: Boolean,
+                   public val obfuscated: Boolean,
+                   public val color: String)
 
-    @Serializable
-    public class ClickEvent(public val action: String, public val value: String)
+//TODO enforce valid translation key
+@Serializable
+public class TranslatableComponent : ChatComponentBase {
+    public var translate: String
+    public val with: List<ChatComponentBase> = mutableListOf()
 
-    @Serializable
-    public class HoverEvent(public val action: String, public val value: String)
+    public constructor(translate: String,
+                       style: Style,
+                       clickEvent: ClickEvent,
+                       hoverEvent: HoverEvent) : super(style, clickEvent, hoverEvent) {
+        this.translate = translate
+    }
+}
 
-    public class Builder {
+//TODO enforce valid keybinds
+@Serializable
+public class KeybindComponent : ChatComponentBase {
+    public var keybind: String
 
-        public fun text(text: String, attributes: TextAttributes.() -> Unit = { }) {
+    public constructor(keybind: String,
+                       style: Style,
+                       clickEvent: ClickEvent,
+                       hoverEvent: HoverEvent) : super(style, clickEvent, hoverEvent) {
+        this.keybind = keybind
+    }
+}
 
-        }
+//TODO enforce valid name, objective
+//TODO allow dynamically loading of value
+@Serializable
+public class ScoreComponent : ChatComponentBase {
+    public val name: String
+    public val objective: String
+    public val value: String
+
+    public constructor (
+        name: String,
+        objective: String,
+        value: String,
+        style: Style,
+        clickEvent: ClickEvent,
+        hoverEvent: HoverEvent
+        ) : super(style, clickEvent, hoverEvent) {
+        this.name = name
+        this.objective = objective
+        this.value = value
+    }
+
+    public constructor (
+        name: String,
+        objective: String,
+        style: Style,
+        clickEvent: ClickEvent,
+        hoverEvent: HoverEvent
+    ) : this(name, objective, "", style, clickEvent, hoverEvent)
+}
+
+//TODO parse target selector
+@Serializable
+public class SelectorComponent : ChatComponentBase {
+    public val targetSelector: String
+
+    public constructor (
+        targetSelector: String,
+        style: Style,
+        clickEvent: ClickEvent,
+        hoverEvent: HoverEvent
+    ) : super(style, clickEvent, hoverEvent) {
+        this.targetSelector = targetSelector
+    }
+}
+
+@Serializable
+public class TextAttributes {
+    public fun onShiftClick(insertion: String) {
 
     }
 
-    public class TextAttributes {
-
-        public fun onHoverDisplayText(text: String) {
-
-        }
-
-        public fun onHoverDisplayItem(item: ItemStack) {
-
-        }
-
-        public fun onHoverDisplayEntity(entity: Entity) {
-
-        }
-
-        public fun onClickOpenUrl(url: String) {
-
-        }
-
-        public fun onClickSendMessage(message: String) {
-
-        }
-
-        public fun onClickSuggestCommand(suggestion: String) {
-
-        }
-
-        public fun onClickRun(block: () -> Unit) {
-
-        }
+    public fun onHover(hoverEvent: HoverEvent) {
 
     }
 
+    public fun onClick(clickEvent: ClickEvent) {
+
+    }
+}
+
+@Serializable
+public class ClickEvent(public val action: ClickAction, public val value: String)
+
+@Serializable
+public enum class ClickAction {
+    @SerialName("open_url") OpenURL,
+    @SerialName("run_command") RunCommand,
+    @SerialName("suggest_command") SuggestCommand,
+    @SerialName("change_page") ChangePage,
+    @SerialName("copy_to_clipboard") CopyToClipboard
+}
+
+@Serializable
+public class HoverEvent(public val action: HoverAction, public val value: String)
+
+@Serializable
+public enum class HoverAction {
+    @SerialName("show_text") displayText,
+    @SerialName("show_item") displayItem,
+    @SerialName("show_entity") displayEntity,
 }
