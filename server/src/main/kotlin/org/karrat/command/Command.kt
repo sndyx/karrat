d0add0815a44e742872028f843b517b9c933ee3b
Sandy <sandy@sndy.moe>
2022-03-20 16:41:31 -0400

    Finish internal command system and add example command (`KillCommand.kt`)

--- server/src/main/kotlin/org/karrat/command/Command.kt
+++ server/src/main/kotlin/org/karrat/command/Command.kt
@@ -4,6 +4,9 @@
 
 package org.karrat.command
 
+import org.karrat.Config
+import org.karrat.entity.Player
+import org.karrat.play.stripColor
 import kotlin.reflect.KClass
 
 public inline fun command(literal: String, structure: Command.() -> Unit = { }): Command {
@@ -13,13 +16,20 @@ public inline fun command(literal: String, structure: Command.() -> Unit = { }):
 }
 
 public fun CommandScope.respond(response: String) {
-    // sender.sendMessage(response)
+    if (this is PlayerCommandScope) {
+        sender.sendMessage(response)
+    } else {
+        println(response)
+    }
 }
 
-public open class Command(public val literal: String) {
+public open class Command(public val literal: String?) {
 
     public val subCommands: MutableList<Command> = mutableListOf()
-    public var executor: CommandScope.() -> Unit = { respond("Invalid syntax.") }
+
+    public var globalExecutor: (CommandScope.() -> Unit)? = null
+    public var playerExecutor: (PlayerCommandScope.() -> Unit)? = null
+    public var consoleExecutor: (CommandScope.() -> Unit)? = null
 
     public inline fun command(literal: String, structure: Command.() -> Unit = { }): Command {
         val command = Command(literal)
@@ -39,15 +49,96 @@ public open class Command(public val literal: String) {
         return command
     }
 
-    public fun run(executor: CommandScope.() -> Unit) {
-        this.executor = executor
+    public fun onRun(executor: CommandScope.() -> Unit): Command {
+        globalExecutor = executor
+        return this
+    }
+
+    public fun onRunByPlayer(executor: PlayerCommandScope.() -> Unit): Command {
+        playerExecutor = executor
+        return this
+    }
+
+    public fun onRunByConsole(executor: CommandScope.() -> Unit): Command {
+        consoleExecutor = executor
+        return this
+    }
+
+    public fun run(tokens: List<String>, sender: Player? = null, args: List<Any>) {
+        if (tokens.isNotEmpty()) {
+            var candidateSubCommand: CommandArgument? = null // Possible match if no better fit
+            subCommands.first { subCommand ->  // Iterate through sub-commands.
+                if (subCommand is CommandArgument) {
+                    runCatching {
+                        when (subCommand.type.qualifiedName) {
+                            "kotlin.Boolean" -> tokens[0].lowercase().toBooleanStrict()
+                            "kotlin.Byte" -> tokens[0].toByte()
+                            "kotlin.Short" -> tokens[0].toShort()
+                            "kotlin.Int" -> tokens[0].toInt()
+                            "kotlin.Long" -> tokens[0].toLong()
+                            "kotlin.Float" -> tokens[0].toFloat()
+                            "kotlin.Double" -> tokens[0].toDouble()
+                            "kotlin.String" -> {
+                                candidateSubCommand = subCommand
+                                return@first false
+                                // Check other sub-commands for a match
+                                // before defaulting to String argument.
+                            }
+                            else -> { return@first true } // Unexpected argument type.
+                        }
+                    }.onSuccess {
+                        candidateSubCommand = subCommand
+                    }
+                } else if (subCommand.literal!!.equals(tokens[0], ignoreCase = true)) {
+                    subCommand.run(tokens.drop(1), sender, args)
+                }
+                return@first false
+            }
+            if (candidateSubCommand != null) {
+                candidateSubCommand!!.run(
+                    tokens.drop(1),
+                    sender,
+                    args.toMutableList().apply { add(tokens[0]) }.toList()
+                )
+            } else {
+                sendInvalidSyntaxMessage(sender)
+            }
+        } else {
+            execute(args, sender)
+        }
+    }
+
+    private fun sendInvalidSyntaxMessage(sender: Player?) {
+        if (sender != null) {
+            sender.sendMessage(Config.invalidSyntaxMessage)
+        } else {
+            println(Config.invalidSyntaxMessage.stripColor())
+        }
+    }
+
+    private fun execute(args: List<Any>, sender: Player? = null) {
+        val arguments = Arguments(args)
+        if (sender != null && playerExecutor != null) {
+            playerExecutor!!.invoke(PlayerCommandScope(sender, arguments))
+        } else if(sender == null && consoleExecutor != null) {
+            consoleExecutor!!.invoke(CommandScope(arguments))
+        } else if(globalExecutor != null) {
+            globalExecutor!!.invoke(CommandScope(arguments))
+        } else {
+            sendInvalidSyntaxMessage(sender)
+        }
     }
 
 }
 
 public open class CommandScope(
-    public val args: Arguments,
+    public val args: Arguments
 )
+
+public class PlayerCommandScope(
+    public val sender: Player,
+    args: Arguments
+) : CommandScope(args)
 
 public class Arguments(
     public val values: List<Any>,
@@ -56,14 +147,14 @@ public class Arguments(
     public val size: Int
         get() = values.size
 
-    public inline fun <reified T> get(index: Int): T {
+    public inline operator fun <reified T> get(index: Int): T {
         return values[index] as T
     }
 
 }
 
 public class CommandArgument(
-    type: KClass<*>,
-    label: String,
-    completions: List<String>
-) : Command("\$literal")
+    public val type: KClass<*>,
+    public val label: String,
+    public val completions: List<String>
+) : Command(null)
---
+++ server/src/main/kotlin/org/karrat/command/KillCommand.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright © Karrat - 2022.
+ */
+
+package org.karrat.command
+
+import org.karrat.Server
+
+internal fun killCommand(): Command =
+    command("kill") {
+        argument<String>().onRun {
+            Server.players
+                .first { it.name.equals(args[0], ignoreCase = true) }
+                .remove()
+        }
+    }.onRunByPlayer {
+        sender.remove()
+    }
