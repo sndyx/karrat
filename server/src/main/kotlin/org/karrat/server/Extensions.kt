5dc0fe0d5936350273cd66bb01227e5f5a8c6a5f
Sandy <70289658+sndyx@users.noreply.github.com>
2022-04-12 10:17:52 -0400

    [server] add `Server.mtps()` extension function

--- server/src/main/kotlin/org/karrat/server/Extensions.kt
+++ server/src/main/kotlin/org/karrat/server/Extensions.kt
@@ -7,8 +7,18 @@ package org.karrat.server
 import org.karrat.Config
 import org.karrat.Server
 
+/**
+ * Returns the current tps (ticks per second) value.
+ */
 public fun Server.tps(): Float =
     1000.0f / maxOf((1000 / Config.tps).toLong(), tickTimeMillis)
+    
+/**
+ * Returns the maximum amount of ticks that could take place in a single second,
+ * given an uncapped tickrate. 
+ */
+public fun Server.mtps(): Float =
+    1000.0f / tickTimeMillis
 
 public fun Server.broadcast(message: String): Unit =
     players.forEach { it.sendMessage(message) }
