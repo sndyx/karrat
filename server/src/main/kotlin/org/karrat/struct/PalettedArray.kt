6655d998f4397a5313d08113c7828b6d5c20c10c
Sandy <sandy@sndy.moe>
2022-04-27 21:33:32 -0400

    [server] `PalettedArray.kt`

---
+++ server/src/main/kotlin/org/karrat/struct/PalettedArray.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright © Karrat - 2022.
+ */
+
+package org.karrat.struct
+
+public class PalettedArray<T>(public val size: Int) {
+    
+    public val palette: Palette<T> = Palette()
+    public val data: VarArray<Int> = VarArray64(size, 0)
+    
+    public operator fun get(index: Int): T = palette[data[index]]
+    
+    public operator fun set(index: Int, value: T) {
+        if (!palette.contains(value)) {
+            palette.add(value)
+        }
+        data[index] = palette.indexOf(value)
+    }
+    
+    public inner class Palette<T> {
+    
+        private val set = mutableSetOf<T>()
+        public val size: Int get() = set.size
+        public val bits: Int get() = 32 - set.size.countLeadingZeroBits()
+        
+        public operator fun get(index: Int): T = set.elementAt(index)
+        
+        public fun add(element: T): Boolean {
+            if (32 - (set.size + 1).countLeadingZeroBits() > bits) data.blockSize++
+            return set.add(element)
+        }
+    
+        public fun remove(element: T): Boolean {
+            if (32 - (set.size - 1).countLeadingZeroBits() < bits) data.blockSize--
+            return set.remove(element)
+        }
+    
+        public fun contains(element: T): Boolean = set.contains(element)
+        
+        public fun indexOf(element: T): Int = set.indexOf(element)
+    
+    }
+    
+}
--- server/src/main/kotlin/org/karrat/struct/VarArray.kt
+++ server/src/main/kotlin/org/karrat/struct/VarArray.kt
@@ -9,6 +9,8 @@ package org.karrat.struct
  */
 public interface VarArray<T : Number> {
     
+    public var blockSize: Int
+    
     public operator fun get(index: Int): T
     
     public operator fun set(index: Int, value: T)
@@ -19,16 +21,28 @@ public interface VarArray<T : Number> {
  * A low-memory-impact [VarArray] implementation where data is stored in 64-bit
  * chunks.
  */
-public class VarArray64<T : Number>(public val size: Int, public val blockSize: Int) : VarArray<T> {
+public class VarArray64<T : Number>(public val size: Int, bitsPerValue: Int) : VarArray<T> {
     
     public val chunkCapacity: Int = 64 / blockSize
-    public val chunks: List<Chunk> = List((size + chunkCapacity - 1) / chunkCapacity) { Chunk() }
+    public val chunks: MutableList<Chunk> = MutableList((size - 1) / chunkCapacity + 1) { Chunk() }
+    
+    private var _blockSize: Int = bitsPerValue
+    override var blockSize: Int
+        get() = _blockSize
+        set(value) {
+            _blockSize = value
+            resize()
+        }
     
     override operator fun get(index: Int): T =
-        chunks[(index + chunkCapacity) / chunkCapacity - 1][index.mod(chunkCapacity)]
+        chunks[index / chunkCapacity][index.mod(chunkCapacity)]
     
     override operator fun set(index: Int, value: T) {
-        chunks[(index + chunkCapacity) / chunkCapacity - 1][index.mod(chunkCapacity)] = value
+        chunks[index / chunkCapacity][index.mod(chunkCapacity)] = value
+    }
+    
+    private fun resize() {
+        TODO("This is hard :( ...")
     }
     
     public inner class Chunk {
