b4892fc872cb60fb517c0837aaf3f8412e9d1ec6
Sandy <sandy@sndy.moe>
2022-09-15 08:52:08 -0400

    Fix compiler error caused by implementations of interface using context receivers

--- server/src/main/kotlin/org/karrat/server/Registry.kt
+++ server/src/main/kotlin/org/karrat/server/Registry.kt
@@ -4,12 +4,18 @@
 
 package org.karrat.server
 
+import org.karrat.plugin.Plugin
+
 public interface Registry<T> {
 
     public val list: MutableList<T>
 
+    context(Plugin)
     public fun register(value: T)
+
+    context(Plugin)
     public fun unregister(value: T)
+
     public fun load()
 
 }
